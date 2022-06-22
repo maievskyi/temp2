3a39888e785bedb96a41201b3249afa621386797
maievskyi <maevua2@gmail.com>
2022-06-22 12:59:04 +0300

    Добавьте файлы проекта.

---
+++ temp2/temp2.cpp
@@ -0,0 +1,14 @@
+// temp2.cpp: определяет точку входа для консольного приложения.
+//  no git
+
+#include "stdafx.h"
+#include <stdlib.h>
+
+int main()
+{
+
+	system("pause");
+    return 0;
+	
+}
+
